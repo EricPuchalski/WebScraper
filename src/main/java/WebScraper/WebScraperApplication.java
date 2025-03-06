a69f22223c60dc6f861cd94b23213042fa05674d
Eric Puchalski <ericpuchalski12@gmail.com>
2025-03-05 22:08:46 -0300

    Initial commit

---
+++ src/main/java/WebScraper/WebScraperApplication.java
@@ -0,0 +1,13 @@
+package WebScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScraperApplication.class, args);
+	}
+
+}

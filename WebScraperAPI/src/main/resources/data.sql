3acdaf50f3c82725e7b0c1784af0fbe47271e2be
Eric Puchalski <ericpuchalski12@gmail.com>
2025-09-08 19:12:56 -0300

    feat: module notification
    se agregó el modulo de notificación, servirá para enviar mails cuando baje de precio el favorito de un usuario

--- WebScraperAPI/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO roles(name) VALUES ('ROLE_CLIENT');
-INSERT INTO roles(name) VALUES ('ROLE_ADMIN');
---
+++ WebScraperNotification/src/test/java/WebScraperNotification/com/WebScraperNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package WebScraperNotification.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScraperNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
